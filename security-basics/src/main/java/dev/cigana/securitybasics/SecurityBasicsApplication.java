f9a81b23479fd33bd1ea1cfbd06253670c0c6d40
João Cigana <joaocigana.cigana@gmail.com>
2023-09-16 17:39:23 -0300

    Commit inicial do projeto.

---
+++ security-basics/src/main/java/dev/cigana/securitybasics/SecurityBasicsApplication.java
@@ -0,0 +1,13 @@
+package dev.cigana.securitybasics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityBasicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityBasicsApplication.class, args);
+	}
+
+}
---
+++ security-basics/src/test/java/dev/cigana/securitybasics/SecurityBasicsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.cigana.securitybasics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityBasicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
